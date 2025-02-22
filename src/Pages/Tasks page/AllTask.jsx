bfb96307111199230db11caa28f8eb4acba1ec22
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-02-22 18:09:25 +0600

    all task

--- src/Pages/Tasks page/AllTask.jsx
+++ src/Pages/Tasks page/AllTask.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import Card from "../../Components/HomeComponents/Card";
-import { IoMdAdd } from "react-icons/io";
-import InputData from "../../Components/HomeComponents/InputData";
+// import React, { useState } from "react";
+// import Card from "../../Components/HomeComponents/Card";
+// import { IoMdAdd } from "react-icons/io";
+// import InputData from "../../Components/HomeComponents/InputData";
 
-const AllTask = () => {
-    const [inputDiv,setInputDiv]=useState("hidden")
-  return (
-    <>
-      <div className="">
-        <div className="flex hover:bg-green-400 justify-end w-fit p-4 text-5xl border-white border rounded">
-          <button onClick={()=>setInputDiv('fixed')} className="">
-          <IoMdAdd />
-          </button>
-        </div>
-        <Card home="true" setInputDiv={setInputDiv}></Card>
-      </div>
-      <InputData inputDiv={inputDiv} setInputDiv={setInputDiv}></InputData>
-    </>
-  );
-};
+// const AllTask = () => {
+//     const [inputDiv,setInputDiv]=useState("hidden")
+//   return (
+//     <>
+//       <div className="">
+//         <div className="flex hover:bg-green-400 justify-end w-fit p-4 text-5xl border-white border rounded">
+//           <button onClick={()=>setInputDiv('fixed')} className="">
+//           <IoMdAdd />
+//           </button>
+//         </div>
+//         <Card home="true" setInputDiv={setInputDiv}></Card>
+//       </div>
+//       <InputData inputDiv={inputDiv} setInputDiv={setInputDiv}></InputData>
+//     </>
+//   );
+// };
 
-export default AllTask;
+// export default AllTask;
